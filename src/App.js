bdf85d5024205d93544ac4f6741bac2ee54d963b
Deniz Saganak <saganak.deniz@gmail.com>
2022-12-09 19:11:41 +0100

    add search bar, checkbox, select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import users from './users.json';
+import { useState } from 'react';
+import linkedInLogo from './linkedInLogo.png'
 
 function App() {
+  const [userList, setUserList] = useState(users);
+  const [search, setSearch] = useState('');
+  const [isTeacher, setIsTeacher] = useState(false);
+  const [isStudent, setIsStudent] = useState(false);
+  const [campus, setCampus] = useState('');
+
+
+  const handleSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredUsers = userList.slice().filter(user => {
+    if(user.firstName.toLocaleLowerCase().includes(search.toLocaleLowerCase()) || user.lastName.toLocaleLowerCase().includes(search.toLocaleLowerCase())) return true;
+  });
+
+  const handleIsTeacher =  e =>{
+    setIsTeacher(e.target.checked);
+    const checkIfTeacher = userList.slice().filter(user => user.role === 'teacher');
+    setUserList(checkIfTeacher);
+  };
+
+  const handleIsStudent =  e =>{
+    setIsStudent(e.target.checked);
+    const checkIfStudent = userList.slice().filter(user => user.role === 'student');
+    setUserList(checkIfStudent);
+  };
+
+  const handleCampusChange = e => {
+    setCampus(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Campus Directory</h1>
+
+    <input type='text' value={search} onChange={handleSearch} />
+
+    <label>Teacher</label>
+    <input type='checkbox' checked={isTeacher} onChange={handleIsTeacher} />
+
+    <label>Student</label>
+    <input type='checkbox' checked={isStudent} onChange={handleIsStudent} />
+
+    <label>Campus:</label>
+    <select>
+      {userList.slice().map(user => (
+        <option value={campus} onChange={handleCampusChange} > {user.campus}</option>
+      ))}
+    </select>
+
+    <table>
+      <thead>
+         <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Campus</th>
+            <th>Role</th>
+            <th>Links</th>
+        </tr>
+      </thead>
+
+      <tbody>
+      {filteredUsers.map(user => (
+        <tr key={user.id}>
+            <th>{user.firstName}</th>
+            <th>{user.lastName}</th>
+            <th>{user.campus}</th>
+            <th>{user.role}</th>
+            <th>{user.linkedin ? <a href={user.linkedin}><img height='16' src={linkedInLogo}/></a> : null}</th>
+        </tr> 
+      ))}
+      </tbody>
+    </table>
     </div>
   );
 }
